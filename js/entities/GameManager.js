7a70fedbe5951b6e84974da8c9b4788500db5046
Vincent Lonero <vincent.lonero.16@gmail.com>
2015-04-14 11:25:17 -0700

    Video 42 in progress

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -42,12 +42,12 @@ game.HeroDeathManager = Object.extend({
 game.ExperienceManager = Object.extend({
     init: function(x, y, settings) {
         this.alwaysUpdate = true;
-        this.gameOver = false;
+        this.gameover = false;
     },
     update: function() {
-        if (game.data.win === true && !this.gameOver) {
+        if (game.data.win === true && !this.gameover) {
             this.gameOver(true);
-        } else if (game.data.win == false && !this.gameOver) {
+        } else if (game.data.win == false && !this.gameover) {
             this.gameOver(false);
         }
         return true;
