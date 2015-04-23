1b07ba3860b9d51e1c47ff4e44aa85f8be144bff
Vincent Lonero <vincent.lonero.16@gmail.com>
2015-04-23 10:53:27 -0700

    Video 49 completed pt. 2

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -13,7 +13,7 @@ game.GameTimerManager = Object.extend({
     },
     addGold: function() {
         if (Math.round(this.now / 1000) % 20 === 0 && (this.now - this.lastCreep >= 1000)) {
-            game.data.gold += 1;
+            game.data.gold += (game.data.exp1 + 1);
             console.log("Current gold:" + game.data.gold);
         }
     },
@@ -63,4 +63,83 @@ game.ExperienceManager = Object.extend({
         this.gameOver = true;
         me.save.exp = game.data.exp;
     }
+});
+
+game.SpendGold = Object.extend({
+    init: function(x, y, settings) {
+        this.now = new Date().getTime();
+        this.lastBuy = new Date().getTime();
+        this.paused = false;
+        this.alwaysUpdate = true;
+        this.updateWhenPaused = true;
+        this.buying = false;
+    },
+    
+    update: function(){
+        this.now = new Date().getTime();
+        if(me.input.isKeyPressed("buy") && this.now-this.lastBuy >= 1000){
+            this.lastBuy = this.now;
+            if(!this.buying){
+                this.startBuying();
+            }else{
+                this.stopBuying();
+            }
+            
+        }
+        return true;
+    },
+    
+    startBuying: function(){
+        this.buying = true;
+        me.state.pause(me.state.PLAY);
+        game.data.pausePos = me.game.viewport.localToWorld(0, 0);
+        game.data.buyscreen = new me.Sprite(game.data.pausePos.x, game.data.pausePos.y, me.loader.getImage("gold-screen"));
+        game.data.buyscreen.updateWhenPaused = true;
+        game.data.buyscreen.setOpacity(0.8);
+        me.game.world.addChild(game.data.buyscreen, 34);
+        game.data.player.body.setVelocity(0, 0);
+        me.input.bindKey(me.input.KEY. F1, "F1", true);
+        me.input.bindKey(me.input.KEY. F2, "F2", true);
+        me.input.bindKey(me.input.KEY. F3, "F3", true);
+        me.input.bindKey(me.input.KEY. F4, "F4", true);
+        me.input.bindKey(me.input.KEY. F5, "F5", true);
+        me.input.bindKey(me.input.KEY. F6, "F6", true);
+        this.setBuyText();
+    },
+    
+    setBuyText: function(){
+        game.data.buytext = new (me.Renderable.extend({
+            init: function() {
+                this._super(me.Renderable, 'init', [game.data.pausePos.x, game.data.pausePos.y, 300, 50]);
+                this.font = new me.Font("Arial", 26, "white");
+                this.updateWhenPaused = true;
+                this.alwaysUpdate;
+            },
+            draw: function(renderer) {
+                this.font.draw(renderer.getContext(), "Press F1-F6 to Buy, Press B to Exit.  Current Gold: " + game.data.gold, this.pos.x, this.pos.y);
+                this.font.draw(renderer.getContext(), "Skill 1: Increase Health. Current Level: " + game.data.exp1 + "Cost: " + ((game.data.exp1) * 10), this.pos.x, this.pos.y + 40);
+                this.font.draw(renderer.getContext(), "Skill 2: Increase Speed. Current Level: " + game.data.exp2 + "Cost: " + ((game.data.exp2) * 10), this.pos.x, this.pos.y + 80);
+                this.font.draw(renderer.getContext(), "Skill 3: Increase Damage. Current Level: " + game.data.exp3 + "Cost: " + ((game.data.exp3) * 10), this.pos.x, this.pos.y + 120);
+                this.font.draw(renderer.getContext(), "Q Ability: Speed Burst! Current Level: " + game.data.ability2 + "Cost: " + ((game.data.ability1+1) * 10), this.pos.x, this.pos.y + 160);
+                this.font.draw(renderer.getContext(), "W Ability: Eat Creep for Health! Current Level:" + game.data.ability2 + "Cost: " + ((game.data.ability2+1) * 10), this.pos.x, this.pos.y + 200);
+                this.font.draw(renderer.getContext(), "E Ability: Throw your Spear! Current Level:" + game.data.ability3 + "Cost: " + ((game.data.ability3+1) * 10), this.pos.x, this.pos.y + 240);
+            }
+        }));
+        me.game.world.addChild(game.data.buytext, 35);
+    },
+    
+    stopBuying: function(){
+        this.buying = false;
+        me.state.resume(me.state.PLAY);
+        game.data.player.body.setVelocity(game.data.playerMoveSpeed, 20);
+        me.game.world.removeChild(game.data.buyscreen);
+        me.input.unbindKey(me.input.KEY. F1, "F1", true);
+        me.input.unbindKey(me.input.KEY. F2, "F2", true);
+        me.input.unbindKey(me.input.KEY. F3, "F3", true);
+        me.input.unbindKey(me.input.KEY. F4, "F4", true);
+        me.input.unbindKey(me.input.KEY. F5, "F5", true);
+        me.input.unbindKey(me.input.KEY. F6, "F6", true);
+        me.game.world.removeChild(game.data.buytext);
+    },
+  
 });
